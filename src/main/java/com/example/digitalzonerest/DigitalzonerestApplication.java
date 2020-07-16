83e143ddd7d713ac66117788cb4c890fabba6b36
AtaySultangaziev <ataysultangaziev2019@gmail.com>
2020-07-16 17:17:06 +0600

    first commit

---
+++ src/main/java/com/example/digitalzonerest/DigitalzonerestApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalzonerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalzonerestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalzonerestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digitalzonerest/DigitalzonerestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalzonerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalzonerestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
